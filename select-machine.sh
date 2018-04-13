c06c2a1d654d68b66a6d997deec5648cdf85d6f8
Wolfereign <nvonwolff@gmail.com>
2018-04-13 13:53:55 -0600

    fixes

--- select-machine.sh
+++ select-machine.sh
@@ -2,67 +2,31 @@
 
 machine() {
 
-    shopt -s extglob nullglob
-
-    Set Script Root Directory Variable
+    # Set Script Root Directory Variable
     basedir="${BASH_SOURCE%/*}"
     if [[ ! -d "$basedir" ]]; then basedir="$PWD"; fi
 
-    # You may omit the following subdirectories
-    # the syntax is that of extended globs, e.g.,
-    # omitdir="cmmdm|not_this_+([[:digit:]])|keep_away*"
-    # If you don't want to omit any subdirectories, leave empty: omitdir=
-    omitdir=
+    # Generate Choices
+    echo "The following machine profiles were found; select one:" > /dev/stderr
 
-    # Create array
-    if [[ -z $omitdir ]]; then
-    cdarray=( "$basedir"/*/ )
-    else
-    cdarray=( "$basedir"/!($omitdir)/ )
-    fi
+    # set the prompt used by select, replacing "#?"
+    PS3="Input Number or 'stop': "
 
-    # remove leading basedir:
-    cdarray=( "${cdarray[@]#"$basedir/"}" )
-
-    # remove trailing backslash and insert Exit choice
-    cdarray=( Exit "${cdarray[@]%/}" )
-
-    # At this point you have a nice array cdarray, indexed from 0 (for Exit)
-    # that contains Exit and all the subdirectories of $basedir
-    # (except the omitted ones)
-    # You should check that you have at least one directory in there:
-    if ((${#cdarray[@]}<=1)); then
-        printf 'No subdirectories found. Exiting.\n'
-        exit 1
-    fi
-
-    # Display the menu:
-    printf 'Please choose from the following. Enter 0 to exit.\n'
-    for i in "${!cdarray[@]}"; do
-        printf '   %d %s\n' "$i" "${cdarray[i]}"
-    done
-    printf '\n'
-
-    # Now wait for user input
-    while true; do
-        read -e -r -p 'Your choice: ' choice
-        # Check that user's choice is a valid number
-        if [[ $choice = +([[:digit:]]) ]]; then
-            # Force the number to be interpreted in radix 10
-            ((choice=10#$choice))
-            # Check that choice is a valid choice
-            ((choice<${#cdarray[@]})) && break
+    # allow the user to choose a file
+    select foldername in "${basedir}/machines"/*; do
+        # leave the loop if the user says 'stop'
+        if [[ "$REPLY" == stop ]]; then 
+            exit 1 
         fi
-        printf 'Invalid choice, please start again.\n'
+
+        # complain if no file was selected, and loop to ask again
+        if [[ "$foldername" == "" ]]; then
+            echo "'$REPLY' is not a valid number" > /dev/stderr
+            continue
+        fi
+
+        # now we can return the selected folder
+        echo "$foldername"
+        break
     done
-
-    # At this point, you're sure the variable choice contains
-    # a valid choice.
-    if ((choice==0)); then
-        printf 'Good bye.\n'
-        exit 1
-    fi
-
-    # Now you can work with subdirectory:
-    return "${cdarray[choice]}"
 }
