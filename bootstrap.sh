fe5b7ee05ac44608017ffd8000e2bffc94b10536
Wolfereign <nvonwolff@gmail.com>
2018-06-18 19:31:27 +0000

    syntax fixes 1

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,16 @@
 # Error Handling
 set -euo pipefail
 
+# Get Directory of Running Script
+sroot="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Partition, Format, & Mount OS Disk
 read -p "Do you want to format and mount the BOOT disk? This will erase the disk. (y/n) " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	select fbd in ./scripts/format-boot_disk-*.sh
-	. $fbd
+	select fbd in "${sroot}/scripts/format-boot_disk-*.sh"
+	. "$fbd"
 else
 	echo "Skipping Partitioning/Mounting"
 	echo "Script is proceeding with the assuption that /mnt is fully configured!!"
@@ -16,15 +20,16 @@ fi
 
 # Custom or Default Deployment?
 read -p "Do you want to use the configuration git repository? (y/n) " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	# Deploy Configuration Files
-	. ./scripts/deploy-config_files.sh
+	. "${sroot}/scripts/deploy-config_files.sh"
 
 	# Generate users.nix (if custom config files were deployed)
 	echo "Select users.nix to generate."
-	select usrnix in ./scripts/generate-users-*.sh
-	. $usrnix
+	select usrnix in "${sroot}/scripts/generate-users-*.sh"
+	. "$usrnix"
 
 	# Install System
 	echo "Installing NixOS!! This will take a while."
@@ -39,13 +44,14 @@ else
 
 	# Review/Edit configuration.nix
 	read -p "Do you want to review/edit configuration.nix? (y/n) " -n 1 -r
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		vim /mnt/etc/nixos/configuration.nix
 	fi
 
 	# Review/Edit hardware_configuration.nix
 	read -p "Do you want to review/edit hardware_configuration.nix? (y/n) " -n 1 -r
-	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		vim /mnt/etc/nixos/hardware_configuration.nix
 	fi
 
--- scripts/format-boot_disk-portland.sh
+++ scripts/format-boot_disk-portland.sh
@@ -3,9 +3,13 @@
 # Error Handling
 set -euo pipefail
 
+# Get Directory of Running Script
+ssroot="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Run Basic Format/Mounting For Boot Drive
-. ./format-boot_disk-basic.sh
+. "${ssroot}/format-boot_disk-basic.sh"
 
 # Mount BTRFS Raid/Pool
 btrfs device scan
+mkdir -p /mnt/butters
 mount -o autodefrag -L butters /mnt/butters
