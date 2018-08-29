4db8cb5c110bc873c3dced5af2a51021a1c29f9d
Wolfereign <Wolfereign@users.noreply.github.com>
2018-08-29 11:53:51 -0600

    fix linking path

--- scripts/deploy-config_files.sh
+++ scripts/deploy-config_files.sh
@@ -15,20 +15,20 @@ nix-env -i git
 # Generate Default Configs
 nixos-generate-config --root /mnt
 
-# Get Desired Machine Configuration
-echo "The following machine profiles were found; select one:"
-select machine in "$RepoRoot"/machines/*/; do break; done
-
 # Clone nixos-configs Repository To Installation
 git -C /mnt/etc/nixos/ clone https://github.com/Wolfereign/nixos-configs.git 
 chmod -R 700 /mnt/etc/nixos/nixos-configs
 
+# Get Desired Machine Configuration
+echo "The following machine profiles were found; select one:"
+select machine in /mnt/etc/nixos/nixos-configs/machines/*/; do break; done
+
 # Link configuration.nix
-if [ -e "/mnt/etc/nixos/nixos-configs/machines/${machine}/configuration.nix" ]; then
-    ln -rsf "/mnt/etc/nixos/nixos-configs/machines/${machine}/configuration.nix" /mnt/etc/nixos/configuration.nix
+if [ -e "${machine}/configuration.nix" ]; then
+    ln -rsf "${machine}/configuration.nix" /mnt/etc/nixos/configuration.nix
 fi
 
 # Link hardware_configuration.nix
-if [ -e "/mnt/etc/nixos/nixos-configs/machines/${machine}/hardware_configuration.nix" ]; then
-    ln -rsf "/mnt/etc/nixos/nixos-configs/machines/${machine}/hardware_configuration.nix" /mnt/etc/nixos/hardware_configuration.nix
+if [ -e "${machine}/hardware_configuration.nix" ]; then
+    ln -rsf "${machine}/hardware_configuration.nix" /mnt/etc/nixos/hardware_configuration.nix
 fi
